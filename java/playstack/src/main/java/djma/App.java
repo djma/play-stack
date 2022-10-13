6d31ad7c8578be4e118526375c0d937462ca1893
David Ma <ma.davidj@gmail.com>
2022-10-12 17:08:27 -0700

    Upgrade from java 7 to 17

--- java/playstack/src/main/java/djma/App.java
+++ java/playstack/src/main/java/djma/App.java
@@ -4,10 +4,12 @@ package djma;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public record Person(String name, int age) {
+    };
+
+    public static void main(String[] args) {
+        var p = new Person("John", 42);
+        System.out.println("Hello %s!".formatted(p.name()));
     }
 }
